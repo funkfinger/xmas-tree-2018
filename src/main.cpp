a5f81d254c2681c15763b9606dddb406461a16a0
Jay Wiggins <jay@irk.com>
2021-01-02 16:57:49 -0700

    esp32 ota update....

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 // code based on https://gist.github.com/kriegsman/1f7ccbbfa492a73c015e
 
+#include "settings.h"
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 #include <FastLED.h>
 
 #define BRIGHTNESS 255
@@ -10,6 +18,10 @@
 
 #define LED_TYPE WS2811
 #define COLOR_ORDER RGB
+
+const char *ssid = SETTINGS_NETWORK_SSID;
+const char *password = SETTINGS_NETWORK_PASS;
+
 CRGB leds[NUM_LEDS];
 
 CRGBPalette16 currentPalette(CRGB::Black);
@@ -34,6 +46,67 @@ void setup()
   delay(1000); // power-up safety delay
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
+
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname(SETTINGS_HOSTNAME);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() {
+        Serial.println("\nEnd");
+      })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          Serial.println("End Failed");
+      });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void ChangePalettePeriodically()
@@ -66,20 +139,129 @@ void addGlitter(fract8 chanceOfGlitter)
   }
 }
 
+/*
+
+CRGBPalette16 pacifica_palette_1 =
+    {0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117,
+     0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x14554B, 0x28AA50};
+CRGBPalette16 pacifica_palette_2 =
+    {0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117,
+     0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x0C5F52, 0x19BE5F};
+CRGBPalette16 pacifica_palette_3 =
+    {0x000208, 0x00030E, 0x000514, 0x00061A, 0x000820, 0x000927, 0x000B2D, 0x000C33,
+     0x000E39, 0x001040, 0x001450, 0x001860, 0x001C70, 0x002080, 0x1040BF, 0x2060FF};
+
+// Add extra 'white' to areas where the four layers of light have lined up brightly
+void pacifica_add_whitecaps()
+{
+  uint8_t basethreshold = beatsin8(9, 55, 65);
+  uint8_t wave = beat8(7);
+
+  for (uint16_t i = 0; i < NUM_LEDS; i++)
+  {
+    uint8_t threshold = scale8(sin8(wave), 20) + basethreshold;
+    wave += 7;
+    uint8_t l = leds[i].getAverageLight();
+    if (l > threshold)
+    {
+      uint8_t overage = l - threshold;
+      uint8_t overage2 = qadd8(overage, overage);
+      leds[i] += CRGB(overage, overage2, qadd8(overage2, overage2));
+    }
+  }
+}
+
+// Deepen the blues and greens
+void pacifica_deepen_colors()
+{
+  for (uint16_t i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i].blue = scale8(leds[i].blue, 145);
+    leds[i].green = scale8(leds[i].green, 200);
+    leds[i] |= CRGB(2, 5, 7);
+  }
+}
+
+// Add one layer of waves into the led array
+void pacifica_one_layer(CRGBPalette16 &p, uint16_t cistart, uint16_t wavescale, uint8_t bri, uint16_t ioff)
+{
+  uint16_t ci = cistart;
+  uint16_t waveangle = ioff;
+  uint16_t wavescale_half = (wavescale / 2) + 20;
+  for (uint16_t i = 0; i < NUM_LEDS; i++)
+  {
+    waveangle += 250;
+    uint16_t s16 = sin16(waveangle) + 32768;
+    uint16_t cs = scale16(s16, wavescale_half) + wavescale_half;
+    ci += cs;
+    uint16_t sindex16 = sin16(ci) + 32768;
+    uint8_t sindex8 = scale16(sindex16, 240);
+    CRGB c = ColorFromPalette(p, sindex8, bri, LINEARBLEND);
+    leds[i] += c;
+  }
+}
+
+void pacifica_loop()
+{
+  // Increment the four "color index start" counters, one for each wave layer.
+  // Each is incremented at a different speed, and the speeds vary over time.
+  static uint16_t sCIStart1, sCIStart2, sCIStart3, sCIStart4;
+  static uint32_t sLastms = 0;
+  uint32_t ms = GET_MILLIS();
+  uint32_t deltams = ms - sLastms;
+  sLastms = ms;
+  uint16_t speedfactor1 = beatsin16(3, 179, 269);
+  uint16_t speedfactor2 = beatsin16(4, 179, 269);
+  uint32_t deltams1 = (deltams * speedfactor1) / 256;
+  uint32_t deltams2 = (deltams * speedfactor2) / 256;
+  uint32_t deltams21 = (deltams1 + deltams2) / 2;
+  sCIStart1 += (deltams1 * beatsin88(1011, 10, 13));
+  sCIStart2 -= (deltams21 * beatsin88(777, 8, 11));
+  sCIStart3 -= (deltams1 * beatsin88(501, 5, 7));
+  sCIStart4 -= (deltams2 * beatsin88(257, 4, 6));
+
+  // Clear out the LED array to a dim background blue-green
+  fill_solid(leds, NUM_LEDS, CRGB(2, 6, 10));
+
+  // Render each of four layers, with different scales and speeds, that vary over time
+  pacifica_one_layer(pacifica_palette_1, sCIStart1, beatsin16(3, 11 * 256, 14 * 256), beatsin8(10, 70, 130), 0 - beat16(301));
+  pacifica_one_layer(pacifica_palette_2, sCIStart2, beatsin16(4, 6 * 256, 9 * 256), beatsin8(17, 40, 80), beat16(401));
+  pacifica_one_layer(pacifica_palette_3, sCIStart3, 6 * 256, beatsin8(9, 10, 38), 0 - beat16(503));
+  pacifica_one_layer(pacifica_palette_3, sCIStart4, 5 * 256, beatsin8(8, 10, 28), beat16(601));
+
+  // Add brighter 'whitecaps' where the waves lines up more
+  pacifica_add_whitecaps();
+
+  // Deepen the blues and greens a bit
+  pacifica_deepen_colors();
+}
+
+*/
+
 void loop()
 {
+  ArduinoOTA.handle();
+
   ChangePalettePeriodically();
   uint8_t maxChanges = 40; //   - meaningful values are 1-48.  1=veeeeeeeery slow, 48=quickest
   nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);
 
   static uint8_t startIndex = 0;
-  startIndex = startIndex - 2; /* motion speed */
+  startIndex = startIndex - 2; // motion speed
   static uint8_t numOfPaletteRepeats = 1;
   static uint8_t paletteBrightness = 100;
 
   fill_palette(leds, NUM_LEDS, startIndex, numOfPaletteRepeats, currentPalette, paletteBrightness, currentBlendType);
 
   addGlitter(60);
+
+  /*
+  EVERY_N_MILLISECONDS(20)
+  {
+    pacifica_loop();
+    FastLED.show();
+  }
+*/
 
   FastLED.show();
   // FastLED.delay(5);
